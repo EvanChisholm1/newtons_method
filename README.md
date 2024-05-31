6e2ed9f56dfd1ee88580798a57829cbccc37515a
Evan Chisholm <elchisholm248@gmail.com>
2024-05-30 21:51:57 -0400

    generalized to any polynomial with a postive power

---
+++ README.md
@@ -0,0 +1,3 @@
+# Newton's Method Root Finder
+
+A simple little C program that finds a root of a function using Newton's method.
--- main.c
+++ main.c
@@ -1,22 +1,67 @@
 #include<stdio.h>
 #include<math.h>
+#include<malloc.h>
 
-float f(float x) {
-    return pow(x, 3) - 5 * pow(x, 2) + x + 9;
+double poly(double *coefficients, int degree, double x) {
+    double result = 0;
+
+    for (int i = 0; i <= degree; i++) {
+        result += coefficients[i] * pow(x, degree - i);
+    }
+
+    return result;
 }
 
-float f_prime(float x) {
-    return 3 * pow(x, 2) - 10 * x + 1;
+double poly_prime(double *coefficients, int degree, double x) {
+    double result = 0;
+
+    for (int i = 0; i < degree; i++) {
+        result += coefficients[i] * (degree - i) * pow(x, degree - i - 1);
+    }
+
+    return result;
+}
+
+int print_eqn(double *coefficients, int degree) {
+    for (int i = 0; i <= degree; i++) {
+        if(coefficients[i] == 0.0) continue;
+        else if(degree - i == 0) {
+            printf("+ %lf", coefficients[i]);
+        } else if(i == 0) {
+            printf("%lfx^%d ", coefficients[i], (degree - i));
+        } else {
+            printf("+ %lfx^%d ", coefficients[i], (degree - i));
+        }
+    }
+
+    printf("\n");
 }
 
 int main() {
-    float x, x_new;
+    int degree;
+    printf("Enter degree of the polynomial: ");
+    scanf("%d", &degree);
+
+    double *coefficients = (double *)malloc(sizeof(double) * (degree + 1));
+
+    for(int i = 0; i <= degree; i++) {
+        printf("Enter the coefficint for the %dth term of the polynomial: ", (degree - i));
+        scanf("%lf", &coefficients[i]);
+    }
+
+
+    print_eqn(coefficients, degree);
+
+
+    double x, x_new;
 
     printf("Enter the initial guess: ");
-    scanf("%f", &x);
+    scanf("%lf", &x);
 
     while(1) {
-        x_new = x - f(x) / f_prime(x);
+        x_new = x - poly(coefficients, degree, x) / poly_prime(coefficients, degree, x);
+
+        printf("%lf\n", x_new);
 
         if(fabs(x_new - x) < 0.00001) {
             break;
@@ -25,7 +70,7 @@ int main() {
         x = x_new;
     }
 
-    printf("The root of the equation is: %f\n", x_new);
+    printf("The root of the equation is: %lf\n", x_new);
 
     return 0;
 }
