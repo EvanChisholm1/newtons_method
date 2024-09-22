869252a2044f71a7da9feeca63176d2a16bcef03
Evan Chisholm <elchisholm248@gmail.com>
2024-09-21 23:01:35 -0400

    removed the need to explicitly say the current degree of the polynomial ;

--- newton.ml
+++ newton.ml
@@ -1,11 +1,11 @@
 let mesage = "Hello, World!"
 let () = print_endline mesage
 
-let rec poly (coeff: float list) (degree: float) (x: float) = 
+let rec poly (coeff: float list) (x: float) = 
   match coeff with
   | [] -> 0.0
-  | c :: r -> (c *. x ** degree) +. poly r (degree -. 1.0) x;;
+  | c :: r -> (c *. x ** float_of_int (List.length r)) +. poly r x;;
 
-let y = poly [1.0; 1.0; 0.0; 0.0] 3.0 2.0;;
+let y = poly [1.0; 1.0; 0.0; 0.0] 2.0;;
 
 Printf.printf "%f" y;;
